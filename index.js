5476ebf59efcb96c409923e50f88206dac66869f
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-24 10:44:24 -0400

    commit tree b08b4cdd8ca32f097fc9f7efb6706bd3a75abf19

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result;
+  if (ride <= 400) {
+    result = 'This one is on me!'}
+      else if (ride > 2500) {
+      result = 'No can do.'}
+        else if (ride > 2000) { 
+        result = 'I will gladly take your thirty bucks.'}
+    return result; 
+        }
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  } 
 }
